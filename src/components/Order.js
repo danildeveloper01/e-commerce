5cf646dd145e84149394dd0de40567ba2d55af24
danildeveloper01 <141183357+danildeveloper01@users.noreply.github.com>
2023-08-02 19:02:16 +0200

    Update Order.js

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,7 @@ export class Order extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={'/img/' + this.props.item.img} />
+        <img src={'../public/img/' + this.props.item.img} />
         <h2>{this.props.item.title}</h2>
         <b>{this.props.item.price}$</b>
         <FaTrash className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)} />
